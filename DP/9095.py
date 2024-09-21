ced0746421f4a499ad8cdeece3883426fff8f454
hoffmuz <h.offthatmuz@gmail.com>
2024-09-21 23:17:08 +0900

    Daily coding test commit : (date +'%Y-%m-%d')

--- DP/9095.py
+++ DP/9095.py
@@ -2,11 +2,11 @@ import sys
 
 inputLines = sys.stdin.read()
 data = list(map(int, inputLines.strip().split()))
-T = data.pop(0)
-maxValue = max(data)
+# T = data.pop(0)
+maxValue = max(data[1:])
 dp = [0] * (maxValue + 1)
 dp[1], dp[2], dp[3] = 1, 2, 4
 for i in range(4, maxValue + 1):
     dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
-for x in data:
+for x in data[1:]:
     print(dp[x])
