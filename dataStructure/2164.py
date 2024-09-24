99eb73be4088103bd1291d3a4536488e9b17e358
hoffmuz <h.offthatmuz@gmail.com>
2024-09-24 12:30:13 +0900

    Daily coding test commit : (date +'%Y-%m-%d')

--- dataStructure/2164.py
+++ dataStructure/2164.py
@@ -1,9 +1,8 @@
 from collections import deque
 
 N = int(input())
-deq = deque([i for i in range(1, N + 1)])
+deq = deque(range(1, N + 1))
 while len(deq) > 1:
     deq.popleft()
-    x = deq.popleft()
-    deq.append(x)
+    deq.append(deq.popleft())
 print(deq[0])
